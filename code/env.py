3768d8318b3ecfb8b15bd76491fc402d72df8c7f
laetitia-teo <laetitia.teodorescu@gmail.com>
2019-07-29 13:51:18 +0000

    first commit

---
+++ code/env.py
@@ -0,0 +1,348 @@
+"""
+This file defines the environment in which the agent can learn to understand
+instructions. It is composed of a grid world in which exist several shapes of
+different colors. The goal of the agent is to displace the shpaes in order to
+form bigger shapes.
+
+The actions that can be taken by the agent are : picking up a shape at position
+(x, y) and displacing it at position (x', y'). The positions can be represented
+as floats and rounded to the closest integer value by the environment.
+
+There are four cardinal directions : north, south, east, west.
+
+Maybe define a grammar for generating instructions ?
+
+Walking in Terry Winograd's footsteps...
+"""
+
+import numpy as np 
+import cv2
+import matplotlib.pyplot as plt
+
+class GridLUImage():
+    """
+    Base class for the GridLU world, implementing the low-level graphical
+    methods generating the environmant images.
+    """
+
+    def __init__(self,
+                 gridsize=5,
+                 cellsize=15):
+        self.gridsize = gridsize
+        self.cellsize = cellsize
+        size = gridsize * cellsize
+        self.grid = np.zeros((size, size, 3))  # 3 rgb channels
+
+        self.RED = np.array([1., 0., 0.])
+        self.GREEN = np.array([0., 1., 0.])
+        self.BLUE = np.array([0., 0., 1.])
+
+        self.PASSIVE = 0.3
+        self.ACTIVE = 0.6
+
+        self.colordict = {'blue': self.BLUE,
+                          'red': self.RED,
+                          'green': self.GREEN}
+        self.shape_pos_list = []
+
+        self.agent_pos = np.array([0, 0])
+        self.agent_state = 'passive'
+
+    def reset_grid(self):
+        self.grid = np.zeros((size, size, 3))
+        self.shape_pos_list = []
+
+    def pos_to_coords(self, pos):
+        n, m = int(pos[0]), int(pos[1])
+        assert(n < self.gridsize and m < self.gridsize)
+        assert(n >= 0 and m >= 0)
+        lowx, highx = self.cellsize * n, self.cellsize * (n+1)
+        lowy, highy = self.cellsize * m, self.cellsize * (m+1)
+        return lowx, highx, lowy, highy
+
+    def is_empty(self, pos):
+        """
+        Returns True if the spot at pos=(n, m) is empty (black)
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        return not self.grid[lowx:highx, lowy:highy, :].any()
+
+    def contains_shape(self, pos):
+        """
+        Returns True if the spot at pos contains no shape (can contain an agent)
+        Assumes a shape does not occupy the totality of a cell.
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        # to check for the presence of a shape, we test if the cell has
+        # uniform color. If not, there is a shape there.
+        cell = self.grid[lowx:highx, lowy:highy, :]
+        one_color = cell[0, 0]  # color vector
+        return (cell - one_color).any()
+   
+    def contains_only_agent(self, pos):
+        """
+        Returns True if the spot at pos contains only an agent (no shape).
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        cell = self.grid[lowx:highx, lowy:highy, :]
+        one_color = cell[0, 0]  # color vector
+        print('foo')
+        return one_color.any() and not (cell - one_color).any()
+
+    def make_shape(self, shape, color=np.ones(3)):
+        """
+        Returns a shape of the specified shape and color.
+        """
+        x, y = np.meshgrid(np.arange(self.cellsize), np.arange(self.cellsize))
+        mid = int(len(x)/2)/len(x)
+        x = x/len(x)
+        y = y/len(y)
+
+        if shape == 'circle':
+            cond = lambda x, y: np.less_equal((x - mid)**2 + (y - mid)**2, 1/4)
+        if shape == 'square':
+            padding = 1/8
+            cond = lambda x, y: np.less_equal(np.maximum(abs(x - mid),
+                                                         abs(y - mid)),
+                                              1/2 - padding)
+        if shape == 'triangle':
+            cond = lambda x, y: np.logical_and( \
+                np.greater_equal((y - mid), -2 * (x - mid) - 1/2), \
+                np.greater_equal((y - mid), 2 * (x - mid) - 1/2))
+        if shape == 'diamond':
+            cond = lambda x, y: np.less_equal(abs(x - mid) + abs(y - mid), 1/2)
+
+        mat = np.where(cond(x, y), 1, 0)
+        mat = np.array([color[0] * mat, color[1] * mat, color[2] * mat])
+        mat = np.swapaxes(mat, 0, -1)  # channels last
+        return mat
+
+    def insert_agent(self, pos, agent_state='passive'):
+        """
+        Inserts a passive agent sprite at position pos.
+        If there is already an agent sprite at this position, does nothing.
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        coeff = 0.0
+        if agent_state == 'passive':
+            coeff = self.PASSIVE
+        if agent_state == 'active':
+            coeff = self.ACTIVE
+        shape = coeff * np.logical_not( \
+            np.sum(self.grid[lowx:highx, lowy:highy, :], axis=-1))
+        shape = np.expand_dims(shape, axis=-1)
+        shape_cat = np.concatenate((shape, shape, shape), axis=-1)
+        print(shape_cat.shape)
+        self.grid[lowx:highx, lowy:highy, :] += shape_cat
+        self.shape_pos_list.append(pos)
+        self.agent_pos = np.array(pos)
+
+    def delete_agent(self, pos, agent_state):
+        """
+        Delete a passive agent sprite at position pos.
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        coeff = 0.0
+        if agent_state == 'passive':
+            coeff = self.PASSIVE
+        if agent_state == 'active':
+            coeff = self.ACTIVE
+        shape = np.sum(self.grid[lowx:highx, lowy:highy, :] \
+            - coeff, axis=-1)
+        shape = np.logical_not(shape)
+        shape = np.expand_dims(shape, axis=-1)
+        shape = coeff * np.concatenate([shape, shape, shape], axis=-1)
+        self.grid[lowx:highx, lowy:highy, :] -= shape
+        self.shape_pos_list.remove(pos)
+
+    def insert_shape(self, shape, pos, color=np.ones(3)):
+        """
+        Inserts the specified shape at position pos.
+
+        If shape is a matrix, this matrix is inserted.
+        If shape is a string, a shape is created according to the specified
+        shape and color.
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        print('shape : %s, pos : %s' % (shape, pos))
+        print(self.shape_pos_list)
+        if type(shape) == str:
+            shape = self.make_shape(shape, color)
+        if not self.is_empty(pos):
+            print(self.retrieve_shape(pos))
+            if self.contains_only_agent(pos):
+                print('bar')
+                self.delete_agent(pos, self.agent_state)
+                self.grid[lowx:highx, lowy:highy, :] = shape
+                self.shape_pos_list.append(pos)
+                self.insert_agent(pos, self.agent_state)
+            else:
+                raise Warning('trying to insert a shape into a non-empty space, ' \
+                    + 'command ignored')
+                return
+        else:
+            self.grid[lowx:highx, lowy:highy, :] = shape
+            self.shape_pos_list.append(pos)
+        print('inserted')
+        print(self.shape_pos_list)
+
+    def delete_shape(self, pos):
+        """
+        Deletes all shape, agent included.
+        """
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        self.grid[lowx:highx, lowy:highy, :] \
+            = np.zeros((self.cellsize, self.cellsize, 3))
+        self.shape_pos_list.remove(pos)
+
+    def retrieve_shape(self, pos):
+        lowx, highx, lowy, highy = self.pos_to_coords(pos)
+        return np.array(self.grid[lowx:highx, lowy:highy, :])
+
+    def move_shape(self, pos1, pos2):
+        """
+        Moves a shape from pos1 to pos2.
+        If there is no shape at pos1, does nothing (raises a Warning).
+        If there is already a shape at pos 2, does nothing (raises a Warning).
+        """
+        if self.is_empty(pos1):
+            raise Warning('no shape to move at pos %s' % pos1)
+            return
+        if not self.is_empty(pos2):
+            raise Warning('space at pos %s is not empty' % pos2)
+            return
+        shape = self.retrieve_shape(pos1)
+        self.delete_shape(pos1)
+        self.insert_shape(shape, pos2)
+
+    def insert_random_shapes(self, n_shapes):
+        intposlist = [self.gridsize*pos[0] + pos[1] for pos in \
+            self.shape_pos_list]
+        candidate_positions = [n for n in range(self.gridsize**2) if n not in \
+            intposlist]
+        print(candidate_positions)
+        new_positions = np.random.choice(candidate_positions, n_shapes, False)
+        poss = []
+        for intpos in new_positions:
+            n = int(intpos // self.gridsize)
+            m = int(intpos % self.gridsize)
+            poss.append((n, m))
+        color_array = [self.RED, self.GREEN, self.BLUE]
+        cols = np.random.choice(3, n_shapes)
+        shape_array = ['circle', 'square', 'diamond', 'triangle']
+        shps = np.random.choice(4, n_shapes)
+        for pos, col, shp in zip(poss, cols, shps):
+            self.insert_shape(shape_array[shp], pos, color_array[col])
+
+    def show_grid(self):
+        plt.imshow(self.grid.swapaxes(0, 1))
+        plt.show()
+
+class GridLU(GridLUImage):
+    """
+    Class wrapper implementing the higher level GridLU functionnalities.
+    Interface for the agent actions.
+    """
+
+    def __init__(self):
+        super(GridLU, self).__init__()
+        
+        # self.insert_agent(self.agent_pos, self.agent_state)
+
+        self.LEFT = np.array([-1, 0])
+        self.RIGHT = np.array([1, 0])
+        self.UP = np.array([0, -1])
+        self.DOWN = np.array([0, 1])
+
+        self.action_space = ['move_left',
+                             'move_right',
+                             'move_up',
+                             'move_down',
+                             'interact',
+                             'no_op']
+        self.num_actions = len(self.action_space)
+
+    def is_valid_direction(self, direction):
+        pos = self.agent_pos + direction
+        n, m = int(pos[0]), int(pos[1])
+        return n < self.gridsize and m < self.gridsize \
+            and n >= 0 and m >= 0
+
+    def move(self, direction):
+        if not self.is_valid_direction(direction):
+            return
+        if self.contains_shape(self.agent_pos) and self.agent_state == 'active':
+            # active and carrying a shape
+            if self.is_empty(self.agent_pos + direction):
+                # move only if there is no shape barring the way
+                self.delete_agent(self.agent_pos, self.agent_state)
+                shape = self.retrieve_shape(self.agent_pos)
+                print(shape)
+                self.delete_shape(self.agent_pos)
+                self.agent_pos += direction
+                self.insert_shape(shape, self.agent_pos)
+                self.insert_agent(self.agent_pos, self.agent_state)
+            else:
+                return  # cannot move with the attached shape because target
+                      # location is not empty
+        elif self.agent_state == 'passive':
+            # if the agent is in a passive state, he can move wherever
+            self.delete_agent(self.agent_pos, self.agent_state)
+            self.agent_pos += direction
+            self.insert_agent(self.agent_pos, self.agent_state)
+
+    def move_left(self):
+        self.move(self.LEFT)
+    
+    def move_right(self):
+        self.move(self.RIGHT)
+
+    def move_up(self):
+        self.move(self.UP)
+
+    def move_down(self):
+        self.move(self.DOWN)
+
+    def interact(self):
+        if self.agent_state == 'passive' and self.contains_shape(self.agent_pos):
+            # the agent can only become active to pick up an object
+            self.delete_agent(self.agent_pos, self.agent_state)
+            self.agent_state = 'active'
+            self.insert_agent(self.agent_pos, self.agent_state)
+        elif self.agent_state == 'active':
+            self.delete_agent(self.agent_pos, self.agent_state)
+            self.agent_state = 'passive'
+            self.insert_agent(self.agent_pos, self.agent_state)
+
+    def no_op(self):
+        pass
+
+    def sample_action(self):
+        return np.random.choice(self.num_actions)
+
+    def step(self, action):
+        """
+        Performs the specified action and returns the subsequent state.
+        """
+        if type(action) == str: 
+            instruction = 'self.' + action + '()'
+        if type(action) == int:
+            instruction = 'self.' + self.action_space[action] + '()'
+        eval(instruction)
+        return self.grid
+
+    def get_state(self):
+        return self.grid
+
+# ============================= Test ======================================
+
+# g = GridLU()
+# g.random_start()
+# text = input('type action here: ')
+# while text != 'q':
+#     if text in ['0', '1', '2', '3', '4', '5']:
+#         text = int(text)
+#         g.step(text)
+#         print(g.agent_state)
+#         g.show_grid()
+#     text = input('type action here: ')
---
+++ code/grammar.py
@@ -0,0 +1,279 @@
+"""
+This is the grammar module associated with the GridLU environment.
+
+The simplest grammar is associated with the simplest task : "GridLU-Relations"
+This grammar allows the generation of positive examples for training the
+discriminator reward model.
+
+The grammar must generate two distinct set of objects : a sequence of language
+tokens (for instance natural language) describing the instruction to be 
+parsed by the policy network, and a set of valid realizations of this
+instruction in the form of a list of sequence of commands to be passed to 
+the GridLU environment for the genaration of a set of valid execution 
+example images.
+
+Maybe add seeds to random stuff ?
+"""
+
+import random
+import numpy as np
+from env import GridLU
+
+class BinaryTree():
+
+    def __init__(self, *args):
+        if len(args) < 1 or len(args) > 3:
+            raise Exception('Binary tree takes 1, 2 or 3 arguments, ' \
+                + '%s were given' % len(args))
+        if len(args) == 1:
+            node = args[0]
+            self.node = node
+        if len(args) == 2:
+            node, below = args
+            self.node = node
+            self.below = below
+        if len(args) == 3:
+            node, left, right = args
+            self.node = node
+            self.left = left
+            self.right = right
+
+    def is_leaf(self):
+        try:
+            self.left
+            self.right
+            return False
+        except AttributeError:
+            try:
+                self.below
+                return False
+            except AttributeError:
+                return True
+
+    def is_pre_leaf(self):
+        try:
+            self.left
+            self.right
+            return False
+        except AttributeError:
+            try:
+                self.below
+                return True
+            except AttributeError:
+                return False
+
+    def left_first(self):
+        if self.is_pre_leaf():
+            return self.below.node
+        else:
+            return self.left.left_first() + ' ' + self.right.left_first()
+
+    def conditional_search(self, token, to_find=None):
+        """
+        Finds the set of leaves below token, after having encountered node
+        to_find.
+        """
+        if self.is_pre_leaf():
+            if to_find is None and self.node == token:
+                return (self.below.node,)
+            else:
+                return tuple()
+        else:
+            if self.node == to_find:
+                to_find = None
+            return self.left.conditional_search(token, to_find) \
+                + self.right.conditional_search(token, to_find)
+
+    def anticonditional_search(self, token, to_not_find=None):
+        """
+        Finds the set of leaves below token, knowing we haven't encountered
+        to_not_find.
+        """
+        if self.is_pre_leaf():
+            if to_not_find is not None and self.node == token:
+                return (self.below.node,)                
+            else:
+                return tuple()
+        else:
+            if self.node == to_not_find:
+                to_not_find = None
+            return self.left.anticonditional_search(token, to_not_find) \
+                + self.right.anticonditional_search(token, to_not_find)
+
+    def __repr__(self):
+        if self.is_pre_leaf():
+            return self.below.node + ', ' + self.node
+        else:
+            return self.left.__repr__() + ', ' + self.node + ', ' \
+                + self.right.__repr__()
+
+class RelationsGrammar():
+    """
+    Base class for the GridLU-Relations grammar.
+    """
+    def __init__(self, env=None):
+
+        self.vars = ['s',
+                     'np',
+                     'rel1',
+                     'rel2',
+                     'n1',
+                     'n2',
+                     'v']
+
+        # an effort has been made to express the grammar in Chomsky Normal 
+        # Form, to be able to use a parsing algorithm such as CYK on it.
+
+        self.productions = {'<s>': [('<v>', '<np>')],
+                            '<np>': [('<n1>', '<rel1>'), ('<n2>', '<rel1>')],
+                            '<rel1>': [('<rel2>', '<n2>')],
+                            '<n2>': [('<prep>', '<n>')],
+                            '<n>': [('<color>', '<shape>')],
+                            '<v>': [('move',)],
+                            '<n1>': [('yourself',)],
+                            '<prep>': [('a',)],
+                            '<color>': [('red',), ('green',), ('blue',)],
+                            '<shape>': [('circle',), ('triangle',), ('diamond',), \
+                                ('square',)],
+                            '<rel2>': [('next to',), ('north of',), ('south of',), \
+                                ('west of',), ('east of',)]}
+
+        self.terminals = self.init_terminals()
+
+        if not env:
+            env = GridLU()
+        self.env = env
+
+    def init_terminals(self):
+        t = []
+        for value in self.productions.values():
+            for item in value:
+                if len(item) == 1:
+                    t.append(item[0])
+        return t
+
+    def is_terminal_production(self, token):
+        """
+        Returns True if the given token leads to a terminal production.
+        """
+        return self.productions[token][0][0] in self.terminals
+
+    def random_branch(self, token):
+        if token in self.terminals:
+            return BinaryTree(token)
+        elif self.is_terminal_production(token):
+            return BinaryTree(token, self.random_branch(\
+                random.choice(self.productions[token])[0]))
+        else:
+            left, right = random.choice(self.productions[token])
+            return BinaryTree(token,
+                              self.random_branch(left),
+                              self.random_branch(right))
+
+    def random_tree(self):
+        """
+        Builds a random instruction tree.
+        """
+        return self.random_branch('<s>')
+
+    def random_sentence(self):
+        tree = self.random_tree()
+        return tree.left_first()
+
+    def extract_obj1_specs(self, tree):
+        """
+        Takes a tree as input, and extracts the color and shape of the object
+        to move.
+        """
+        try:
+            color = tree.anticonditional_search('<color>', '<rel1>')[0]
+            shape = tree.anticonditional_search('<shape>', '<rel1>')[0]
+            return color, shape
+        except:
+            return tree.anticonditional_search('<n1>', '<rel1>')
+
+    def extract_obj2_specs(self, tree):
+        """
+        Takes a tree as input, and extracts the color and shape of the stable
+        object.
+        """
+        color = tree.conditional_search('<color>', '<rel1>')[0]
+        shape = tree.conditional_search('<shape>', '<rel1>')[0]
+        return color, shape
+
+    def extract_relation(self, tree):
+        return tree.conditional_search('<rel2>')
+
+    def genarate_image(self, tree=None, n_images=1):
+        """
+        Generates a random instruction, then creates a number n_images
+        of images corresponding to the valid completion of this task.
+        """
+        if not tree:
+            tree = self.random_tree
+        obj2specs = self.extract_obj2_specs(tree)
+        obj1specs = self.extract_obj1_specs(tree)
+        relation = self.extract_relation(tree)[0]
+        sentence = tree.left_first()
+        # generate random position
+        # generate obj1 position according to relation
+        nmin, nmax, mmin, mmax = 0, self.env.gridsize, 0, self.env.gridsize
+        print(nmin, nmax, mmin, mmax)
+        if relation == 'next to':
+            relation = random.choice(['west of', 'east of', 'north of', \
+                'south of'])
+        if relation == 'west of':
+            nmin += 1
+        elif relation == 'east of':
+            nmax -= 1
+        elif relation == 'north of':
+            mmin += 1
+        elif relation == 'south of':
+            mmax -= 1
+        n = np.random.randint(nmin, nmax)
+        m = np.random.randint(mmin, mmax)
+        pos = n, m
+        print(relation)
+        if relation == 'west of':
+            pos_ = n-1, m
+        elif relation == 'east of':
+            pos_ = n+1, m
+        elif relation == 'north of':
+            pos_ = n, m-1
+        elif relation == 'south of':
+            pos_ = n, m+1
+        # populate the grid with those two objects, according to their specsS
+        obj2color, obj2shape = obj2specs
+        obj2color = self.env.colordict[obj2color]
+        self.env.insert_shape(obj2shape, pos, obj2color)
+        if len(obj1specs) == 1:
+            # obj1 is the agent
+            self.env.insert_agent(pos_)
+            agent_inserted = True
+        if len(obj1specs) == 2:
+            obj1color, obj1shape = obj1specs
+            obj1color = self.env.colordict[obj1color]
+            self.env.insert_shape(obj1shape, pos_, obj1color)
+            agent_inserted = False
+        nb_other_shapes = np.random.choice(int(self.env.gridsize**2/2))
+        self.env.insert_random_shapes(nb_other_shapes)
+        if not agent_inserted:
+            pos = np.random.choice(self.env.gridsize, 2)
+            self.env.insert_agent(pos)
+        # actually generate the image
+        self.env.show_grid()
+        return self.env.grid
+        
+    def parse_sentence(self):
+        """
+        Implement the CYK algorithm for parsing.
+        Do not forget to parse e.g. 'north of' as a single token.
+        """
+        return NotImplemented
+
+# ============================ Test ====================================
+
+c = RelationsGrammar()
+t = c.random_tree()
+print(t.left_first())
+c.genarate_image(t)
